2bfa4649a795e857562cdd127ebd8eaa86b98ad4
ohAkse <segassdc1@naver.com>
2024-05-05 20:57:28 +0900

    git ignore 및 프로젝트설정

---
+++ EZMacker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EZMacker
+//
+//  Created by 박유경 on 5/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EZMacker/EZMackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZMackerApp.swift
+//  EZMacker
+//
+//  Created by 박유경 on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EZMackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
