3052d44b8ab72601de4014e283276d6dffd1e37c
ohAkse <segassdc1@naver.com>
2024-05-06 20:10:15 +0900

    듀얼모니터일때 프로그램 이동, 4사분면 기준 이동, 화살표기준 조금씩 이동하게 키바인딩 커밋(커맨드 기준)

--- EZMacker/EZMackerApp.swift
+++ EZMacker/EZMackerApp.swift
@@ -9,11 +9,27 @@ import SwiftUI
 
 @main
 struct EZMackerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainContentView().frame(minWidth: 100, minHeight: 700)
         }
         .windowToolbarStyle(.unifiedCompact)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        window = NSApp.windows.first
         
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+            let modifierFlag = ModifierFlag(event.modifierFlags)
+            let character = KeyboardCharacter(event.keyCode)
+            KeyboardShortcutHandler.handleEvent(modifierFlag: modifierFlag, character: character)
+            return event
+        }
     }
 }
