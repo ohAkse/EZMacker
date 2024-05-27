245d1b9ea90b3087962eb1790ca72c8edb3d5826
ohAkse <segassdc1@naver.com>
2024-05-27 22:24:42 +0900

    CommandToolRunner 추가 및 불필요한 권한 삭제

---
+++ EZMacker/Util/CommandToolRunner.swift
@@ -0,0 +1,34 @@
+//
+//  CommandToolRunner.swift
+//  EZMacker
+//
+//  Created by 박유경 on 5/27/24.
+//
+
+import Foundation
+struct CommandToolRunner {
+    static let shared = CommandToolRunner()
+    func runMDFind() {
+        let process = Process()
+        // Use the correct path to mdfind
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/mdfind")
+        process.arguments = ["-name", "Info.plist"]
+        
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        process.standardError = pipe
+        
+        do {
+            try process.run()
+            
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            if let output = String(data: data, encoding: .utf8) {
+                Logger.writeLog(.info, message: output)
+            }
+        } catch {
+            DispatchQueue.main.async {
+                Logger.writeLog(.error, message: error.localizedDescription)
+            }
+        }
+    }
+}
--- EZMacker/ViewModel/SmartWifiViewModel.swift
+++ EZMacker/ViewModel/SmartWifiViewModel.swift
@@ -7,7 +7,7 @@
 
 import Combine
 import CoreWLAN
-import CoreLocation
+
 class SmartWifiViewModel<ProvidableType: AppSmartWifiServiceProvidable>: ObservableObject {
     
     deinit {
@@ -107,3 +107,111 @@ class SmartWifiViewModel<ProvidableType: AppSmartWifiServiceProvidable>: Observa
     }
 }
 
+
+//import Combine
+//import CoreWLAN
+//import CoreLocation
+//
+//class SmartWifiViewModel<ProvidableType: AppSmartWifiServiceProvidable>: ObservableObject {
+//    
+//    deinit {
+//        Logger.writeLog(.debug, message: "SmartWifiViewModel deinit Called")
+//    }
+//    
+//    private let appSmartWifiService: ProvidableType
+//    private let systemPreferenceService: SystemPreferenceAccessible
+//    private let appCoreWLanWifiService: AppCoreWLANWifiProvidable
+//    
+//    init(appSmartWifiService: ProvidableType, systemPreferenceService: SystemPreferenceAccessible, appCoreWLanWifiService: AppCoreWLANWifiProvidable) {
+//        self.appSmartWifiService = appSmartWifiService
+//        self.appCoreWLanWifiService = appCoreWLanWifiService
+//        self.systemPreferenceService = systemPreferenceService
+//    }
+//    
+//    @Published var channelBandwidth = 0
+//    @Published var channelFrequency = 0
+//    @Published var channel = 0
+//    @Published var band = ""
+//    @Published var ssID = ""
+//    @Published var locale = ""
+//    @Published var currentWifiStrength = ""
+//    @Published var currentTransmitRate = ""
+//    @Published var errorMessage: String?
+//
+//    private var cancellables = Set<AnyCancellable>()
+//    
+//    func requestWifiInfo() {
+//        let channelPublisher = appSmartWifiService.getRegistry(forKey: .IO80211Channel).compactMap { $0 as? Int }
+//        let channelBandwidthPublisher = appSmartWifiService.getRegistry(forKey: .IO80211ChannelBandwidth).compactMap { $0 as? Int }
+//        let channelFrequencyPublisher = appSmartWifiService.getRegistry(forKey: .IO80211ChannelFrequency).compactMap { $0 as? Int }
+//
+//        handleZipPublisher(zip: channelPublisher.zip(channelBandwidthPublisher, channelFrequencyPublisher), updateAction: { [weak self] channel, channelBandwidth, channelFrequency in
+//            self?.channel = channel
+//            self?.channelBandwidth = channelBandwidth
+//            self?.channelFrequency = channelFrequency
+//            Logger.writeLog(.info, message: "\(channel)")
+//            Logger.writeLog(.info, message: "\(channelBandwidth)")
+//            Logger.writeLog(.info, message: "\(channelFrequency)")
+//        })
+//        
+//        let bandPublisher = appSmartWifiService.getRegistry(forKey: .IO80211Band).compactMap { $0 as? String }
+//        let ssIDPublisher = appSmartWifiService.getRegistry(forKey: .IO80211SSID).compactMap { $0 as? String }
+//        let localePublisher = appSmartWifiService.getRegistry(forKey: .IO80211Locale).compactMap { $0 as? String }
+//
+//        handleZipPublisher(zip: bandPublisher.zip(ssIDPublisher, localePublisher), updateAction: { [weak self] band, ssID, locale in
+//            self?.band = band
+//            self?.ssID = ssID
+//            self?.locale = locale
+//            Logger.writeLog(.info, message: band)
+//            Logger.writeLog(.info, message: ssID)
+//            Logger.writeLog(.info, message: locale)
+//        })
+//    }
+//    
+//    func requestCoreWLanWifiInfo() {
+//        handleFuture(future: appCoreWLanWifiService.getSignalStrength(), updateAction: { [weak self] currentWifiStrength in
+//            self?.currentWifiStrength = currentWifiStrength
+//            Logger.writeLog(.info, message: currentWifiStrength)
+//        })
+//        
+//        handleFuture(future: appCoreWLanWifiService.getMbpsRate(), updateAction: { [weak self] currentTransmitRate in
+//            self?.currentTransmitRate = currentTransmitRate
+//            Logger.writeLog(.info, message: currentTransmitRate)
+//        })
+//    }
+//
+//    private func handleZipPublisher<P: Publisher>(zip: P, updateAction: @escaping (P.Output) -> Void) where P.Failure == Never {
+//        zip
+//            .subscribe(on: DispatchQueue.global())
+//            .receive(on: DispatchQueue.main)
+//            .sink(receiveCompletion: { completion in
+//                if case let .failure(error) = completion {
+//                    self.handleError(error)
+//                }
+//            }, receiveValue: updateAction)
+//            .store(in: &cancellables)
+//    }
+//
+//    private func handleFuture<T>(future: Future<T, AppCoreWLanError>, updateAction: @escaping (T) -> Void) {
+//        future
+//            .subscribe(on: DispatchQueue.global())
+//            .receive(on: DispatchQueue.main)
+//            .sink(receiveCompletion: handleCompletion, receiveValue: updateAction)
+//            .store(in: &cancellables)
+//    }
+//
+//    private func handleCompletion(_ completion: Subscribers.Completion<AppCoreWLanError>) {
+//        switch completion {
+//        case .finished:
+//            break
+//        case .failure(let error):
+//            errorMessage = error.localizedDescription
+//            Logger.writeLog(.error, message: error.localizedDescription)
+//        }
+//    }
+//
+//    private func handleError(_ error: Error) {
+//        errorMessage = error.localizedDescription
+//        Logger.writeLog(.error, message: error.localizedDescription)
+//    }
+//}
